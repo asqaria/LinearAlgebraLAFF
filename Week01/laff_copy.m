12376b47138a4a6b9a1e3def1a91a91127782ac2
asqaria <asqaria@hotmail.com>
2018-07-06 18:35:52 +0800

    first homework is done

--- Week01/laff_copy.m
+++ Week01/laff_copy.m
@@ -14,7 +14,6 @@ function [ y_out ] = laff_copy( x, y )
 [ m_x, n_x ] = size( x );
 [ m_y, n_y ] = size( y );
 
-% Make sure x and y are (row or column) vectors of equal length
 if ( m_x ~= 1 & n_x ~= 1 ) | ( m_y ~= 1 & n_y ~= 1 )
     y_out = 'FAILED';
     return
@@ -24,34 +23,28 @@ if ( m_x * n_x ~= m_y * n_y )
     return
 end
 
-if ( n_x == 1 )     % x is a column vector
-    if ( n_y == 1 )     % y is a column vector
-        % Copy the elements of x into the elements of y
-        for i=1:m_x   
-            y( i,1 ) = x( i,1 );
-        end
-    else     % y is a row vector
-        % Copy the elements of x into the elements of y
-        for i=1:m_x   
-            y( 1,i ) = x( i,1 );
-        end
-    end
-else    % x is a row vector
-    if ( n_y == 1 )     % y is a column vector
-        % Copy the elements of x into the elements of y
-        for i=1:n_x   
-            y( i,1 ) = x( 1,i );
-        end
-    else     % y is a row vector
-        % Copy the elements of x into the elements of y
-        for i=1:n_x   
-            y( 1,i ) = x( 1,i );
-        end
-    end
+if(m_x==1) % x row
+   if(m_y==1) % y row
+       for i=1:n_x
+        y(1,i) = x(1,i);
+       end
+   else % y column
+       for i=1:n_x
+        y(i,1) = x(1,i);
+       end
+   end
+else % x column
+   if(m_y==1) % y row
+       for i=1:m_x
+        y(1,i) = x(i,1);
+       end
+   else % y column
+       for i=1:m_x
+        y(i,1) = x(i,1);
+       end
+   end
 end
 
-% Return the updated y in y_out
 y_out = y;
-
 return
 end
