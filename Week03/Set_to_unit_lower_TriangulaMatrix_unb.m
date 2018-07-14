7551802973f5849a028bf11629393e2e832683aa
asqaria <asqaria@hotmail.com>
2018-07-15 01:55:02 +0800

    week 3 files

---
+++ Week03/Set_to_unit_lower_TriangulaMatrix_unb.m
@@ -0,0 +1,34 @@
+function [ A_out ] = Set_to_unit_lower_TriangulaMatrix_unb( A )
+
+  [ ATL, ATR, ...
+    ABL, ABR ] = FLA_Part_2x2( A, ...
+                               0, 0, 'FLA_TL' );
+
+  while ( size( ATL, 1 ) < size( A, 1 ) )
+
+    [ A00,  a01,     A02,  ...
+      a10t, alpha11, a12t, ...
+      A20,  a21,     A22 ] = FLA_Repart_2x2_to_3x3( ATL, ATR, ...
+                                                    ABL, ABR, ...
+                                                    1, 1, 'FLA_BR' );
+
+    %------------------------------------------------------------%
+
+    %                       update line 1                        %
+    %                             :                              %
+    %                       update line n                        %
+
+    %------------------------------------------------------------%
+
+    [ ATL, ATR, ...
+      ABL, ABR ] = FLA_Cont_with_3x3_to_2x2( A00,  a01,     A02,  ...
+                                             a10t, alpha11, a12t, ...
+                                             A20,  a21,     A22, ...
+                                             'FLA_TL' );
+
+  end
+
+  A_out = [ ATL, ATR
+            ABL, ABR ];
+
+return
